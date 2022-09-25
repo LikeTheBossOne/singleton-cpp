0fe0b7f2e97e1c8f57a27298b9443541d5051d70
Brendan Boss <blboss@ncsu.edu>
2022-09-25 17:56:57 -0500

    adding premake and gitignore

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "singleton-cpp"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"*.h",
+		"*.cpp"
+	}
+
+	defines
+	{
+		"singleton_EXPORTS"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
